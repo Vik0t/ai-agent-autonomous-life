08dc89715aa75925c69740d8d1ee302922c15726
Vik0t <viktorvarrar@gmail.com>
2026-02-16 21:49:10 +0700

    refactoring

---
+++ static/main.js
@@ -0,0 +1,5 @@
+// Инициализация приложения
+document.addEventListener('DOMContentLoaded', () => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(React.createElement(App));
+});
